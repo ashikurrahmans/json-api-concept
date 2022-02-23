02f052ec7a844c7f7d5c4382bd5ab52974fd9cde
HackBomm <hackbomm.net@gmail.com>
2022-02-23 09:53:07 +0600

    Design update - JSON API

--- api.js
+++ api.js
@@ -11,8 +11,8 @@ const randomUsers = data =>{
        const pdata = document.createElement('li')
        console.log(user);
        pdata.innerHTML = `
-       <h1> My first Name is : ${user.name.first} ${user.name.last}</h1> 
-       <h2> Also email address is : ${user.email}</h2>`
+       <h1> Name : ${user.name.first} ${user.name.last}</h1> 
+       <h2> Email Address: ${user.email}</h2>`
        userDiv.appendChild(pdata)
        
    }
