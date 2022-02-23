862badbeee894a83c6c2fc148e398946ddfd8c2b
HackBomm <hackbomm.net@gmail.com>
2022-02-23 12:14:00 +0600

    Simplify the come and make it very readable

--- country API/country.js
+++ country API/country.js
@@ -1,9 +1,20 @@
 function loadCountry(){
     fetch('https://restcountries.com/v3.1/all')
-    .then(res = res.json())
-    .then(data = country(data))
+    .then(response => response.json())
+    .then(data => country(data))
 }
 
+loadCountry()
 function country(countries){
-
+    const divCountry = document.getElementById('countries')
+    countries.forEach(country => {
+       const div = document.createElement('div');
+        div.innerHTML =    `
+        <h2>${country.name.common}</h2>
+        <p>${country.capital}</p>
+        <button>Know more</button>
+        `
+        div.classList.add('country')
+        divCountry.appendChild(div)
+    })
 }
