dc0b743cfa064281c657f82d1f8119ef4f49d6f4
HackBomm <hackbomm.net@gmail.com>
2022-02-25 11:36:02 +0600

    Dog API

--- dog API/custom.js
+++ dog API/custom.js
@@ -12,11 +12,18 @@ const displayDog = (doglist) => {
     const first20Dog = doglist.slice(0,20);
     console.log(doglist);
     for(const dog of first20Dog){
-        const div = document.createElement('div')
+        const div = document.createElement('div');
+       div.setAttribute('class','col-lg-4 m-2')   
         console.log(dog);
         div.innerHTML = `
-            <h1>${dog.name}</h1>
-            <h4>${dog.temperament}</h4>
+        <div class="card" style="width: 18rem;">
+                <img src="${dog.image.url}" class="card-img-top" alt="..." width="400px" height="200px">
+            <div class="card-body">
+                <h5 class="card-title">${dog.name}</h5>
+                <p class="card-text">${dog.temperament}</p>
+                <a href="#" class="btn btn-primary">Know more</a>
+            </div>
+        </div>
         `
         section.appendChild(div)
 
